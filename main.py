4adf8f9cae4e59312fe2112152e634f2412b6090
Xarraket7 <said.amankeldin@gmail.com>
2026-03-21 01:38:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ Thread(target=run).start()
 
 # --- Настройки ---
 CHANNEL_ID = 1483887310771847296
+ANNOUNCE_CHANNEL_ID = 1483338410583396474
+
 GIF_URL = "https://cdn.discordapp.com/attachments/1312367256038019092/1483904073110523974/homelander-appreciate-smile.gif_1.gif?ex=69bc48dc&is=69baf75c&hm=45acb995801854b8278e89b893964b82f4a3cd7c73bd0b16c9f8c9bfefb0d0e6&"
 
 intents = discord.Intents.default()
@@ -53,10 +55,10 @@ async def on_member_join(member):
         await channel.send(embed=embed)
 
 # --- Команда /announce ---
-@tree.command(name="announce", description="Отправить объявление с гифкой в канал", guild=discord.Object(id=1483338409677164627))
+@tree.command(name="announce", description="Отправить объявление в общий чат", guild=discord.Object(id=1483338409677164627))
 @app_commands.describe(текст="Текст объявления", гифка="Ссылка на GIF")
 async def announce(interaction: discord.Interaction, текст: str, гифка: str = None):
-    channel = client.get_channel(CHANNEL_ID)
+    channel = client.get_channel(ANNOUNCE_CHANNEL_ID)
     if isinstance(channel, discord.TextChannel):
         embed = discord.Embed(
             title="📢 Объявление!",
@@ -65,7 +67,6 @@ async def announce(interaction: discord.Interaction, текст: str, гифка
         )
         if гифка:
             embed.set_image(url=гифка)
-        embed.set_footer(text=f"Отправил: {interaction.user.name}")
         await channel.send(embed=embed)
         await interaction.response.send_message("✅ Объявление отправлено!", ephemeral=True)
 
